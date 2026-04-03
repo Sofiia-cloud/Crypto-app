b00979023d7303d0bf16602b2cb4dcc2a0524f14
Sofiia-cloud <sofiia.g@icloud.com>
2026-04-03 21:06:07 +0300

    add some blocks to the header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,12 +1,43 @@
-import { Layout } from "antd";
+import { Layout, Select, Space, Button } from "antd";
+import { useCrypto } from "../../context/crypto-context";
 const headerStyle = {
   textAlign: "center",
-  color: "#fff",
   height: 60,
-  paddingInline: 48,
-  lineHeight: "64px",
-  backgroundColor: "#4096ff",
+  width: "100%",
+  padding: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 };
+
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+  const { crypto } = useCrypto();
+  function handleSelect(value) {
+    console.log(value);
+  }
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{ width: 250 }}
+        onSelect={handleSelect}
+        value="Press / to open"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: 20 }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />{" "}
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add Asset</Button>
+    </Layout.Header>
+  );
 }
