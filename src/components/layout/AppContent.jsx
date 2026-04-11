d8d4c3addda16831acda3ac107b33a56214c8415
Sofiia-cloud <sofiia.g@icloud.com>
2026-04-11 20:55:41 +0300

    add new item to the app content

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,5 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../context/crypto-context";
 const contentStyle = {
   textAlign: "center",
   minHeight: "calc(100vh - 60px)",
@@ -7,5 +8,23 @@ const contentStyle = {
   padding: "1rem",
 };
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price;
+    return acc;
+  }, {});
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "white" }}>
+        Portfolio:{" "}
+        {assets
+          .map((asset) => {
+            return asset.amount * cryptoPriceMap[asset.id];
+          })
+          .reduce((ass, v) => (ass += v), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+    </Layout.Content>
+  );
 }
